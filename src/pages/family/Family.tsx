0c0a324f585a70882845082706bdfc5d4d64f947
Ádám Horváth <adam.horvath@attrecto.com>
2020-12-24 16:06:20 +0100

    family imrovement

--- src/pages/family/Family.tsx
+++ src/pages/family/Family.tsx
@@ -8,7 +8,7 @@ export const Family: FC<{}> = () => {
   return (
     <Page className={'family-page'} title={'Család'}>
       <div className={'row ml-0 mr-0'}>
-        <div className={'col-12 top-content'}>
+        <div className={'col-12 top-content family-content overflow-auto ml-0'}>
           <img src={FamilyTree} alt={'családfa'} />
         </div>
       </div>
