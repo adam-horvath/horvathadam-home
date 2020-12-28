c6ae2aca64f657c80e7367a68983585d4da6f777
Ádám Horváth <adam.horvath@attrecto.com>
2020-12-28 12:32:55 +0100

    remove debug spider

--- src/util/easterEggs.ts
+++ src/util/easterEggs.ts
@@ -31,8 +31,7 @@ const isDaylight = (sunrise: Moment, sunset: Moment) =>
 
 export const getEasterEggsByDate = () => {
   switch (formatDate(new Date()).substring(5, 10)) {
-    // case '10-31':
-    case '12-28':
+    case '10-31':
       return { spider: true };
     case '04-01':
       return { fool: true };
