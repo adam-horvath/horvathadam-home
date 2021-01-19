328ee0a4f3fe3029793793c03571e62549eaa81b
Ádám Horváth <adam.horvath@attrecto.com>
2021-01-19 18:09:04 +0100

    service worker trial

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import { createBrowserHistory } from 'history';
+import {registerRoute} from 'workbox-routing';
+import * as strategies from 'workbox-strategies';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import * as serviceWorker from 'serviceWorker';
 import i18n from 'i18n';
 import App from 'app/App';
 import 'styles/styles.scss';
@@ -24,3 +27,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
+
+const strategy = new strategies.NetworkFirst();
+
+registerRoute(/\D*\/json\/\D+\.json/gm, strategy);
